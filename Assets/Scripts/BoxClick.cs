7506592c3400dc91413f230a18df9ce0f45cd8d8
puchuanyuan9 <puchuanyuan9@gmail.com>
2026-06-09 10:42:15 +0900

    [update]段ボールをクリックすると段ボールが開閉する仕組みを作った

--- Assets/Scripts/BoxClick.cs
+++ Assets/Scripts/BoxClick.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BoxClick : MonoBehaviour
 {
@@ -10,9 +11,20 @@ public class BoxClick : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    void OnMouseDown()
+    void Update()
     {
-        isOpen = !isOpen;
-        animator.SetBool("IsOpen", isOpen);
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.transform == transform || hit.transform.IsChildOf(transform))
+                {
+                    isOpen = !isOpen;
+                    animator.SetBool("IsOpen", isOpen);
+                }
+            }
+        }
     }
 }
