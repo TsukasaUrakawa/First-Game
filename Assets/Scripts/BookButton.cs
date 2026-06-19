392629e1a00b16001e99aee8f09fd8dd88f4f207
puchuanyuan9 <puchuanyuan9@gmail.com>
2026-06-19 15:16:04 +0900

    [update]自分でスクリプトかけるように段ボールの開閉、UIの表示をやり直した

---
+++ Assets/Scripts/BookButton.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookButton : MonoBehaviour
+{
+    private Image _image;
+    
+}
--- Assets/Scripts/BoxOpenAndClose.cs
+++ Assets/Scripts/BoxOpenAndClose.cs
@@ -2,17 +2,31 @@ using UnityEngine;
 
 public class BoxOpenAndClose : MonoBehaviour
 {
+    [SerializeField] GameObject _bookListPanel;
+    private Animator _animator;
+
     private void OnMouseDown()
     {
+        if (_animator.GetBool("IsOpen"))
+        {
+            _animator.SetBool("IsOpen", false);
+        }
+        else
+        {
+            _animator.SetBool("IsOpen", true);
+        }
+
+
 
     }
-    void Start()
+    private void Start()
     {
-
+        _animator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void ShowBookList()
     {
-        
+        _bookListPanel.SetActive(true);
     }
+    
 }
