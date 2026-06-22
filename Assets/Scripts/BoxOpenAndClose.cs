280d75455c4b5d307b9192af98ea65091a1c3a5b
puchuanyuan9 <puchuanyuan9@gmail.com>
2026-06-22 12:04:49 +0900

    [add]UIの画像変更、構成の変更、段ボール開閉音追加

--- Assets/Scripts/BoxOpenAndClose.cs
+++ Assets/Scripts/BoxOpenAndClose.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class BoxOpenAndClose : MonoBehaviour
 {
     [SerializeField] GameObject _bookListPanel;
+    [SerializeField] AudioClip _openSE;
+    [SerializeField] AudioClip _closeSE;
     private Animator _animator;
+    private AudioSource _audioSource;
 
     //Animationの遷移
     private void OnMouseDown()
@@ -13,6 +16,7 @@ public class BoxOpenAndClose : MonoBehaviour
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     private void ShowBookList()
@@ -25,5 +29,22 @@ public class BoxOpenAndClose : MonoBehaviour
         _bookListPanel.SetActive(false);
         _animator.SetBool("IsOpen", false);
     }
-    
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && _bookListPanel.activeSelf)
+        {
+            CloseBookList();
+        }
+    }
+
+    private void PlayOpenSE()
+    {
+        _audioSource.PlayOneShot(_openSE);
+    }
+
+    private void PlayCloseSE()
+    {
+        _audioSource.PlayOneShot(_closeSE);
+    }
 }
