5be64d12a76e4b1f6149ee328f2cc67bf2acd18d
puchuanyuan9 <puchuanyuan9@gmail.com>
2026-07-08 14:59:46 +0900

    [update]手持ちのアイテム欄を更新

--- Assets/Scripts/HandItemSlotUI.cs
+++ Assets/Scripts/HandItemSlotUI.cs
@@ -2,21 +2,36 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class HandItemSlotUI : MonoBehaviour, IPointerClickHandler
+public class HandItemSlotUI : MonoBehaviour,
+    IPointerClickHandler,
+    IPointerEnterHandler,
+    IPointerExitHandler
 {
     [SerializeField] private Image _bookImage;
-    [SerializeField] private Color _selectedColor = new Color(1f, 0.92f, 0.65f, 1f);
+    [SerializeField] private GameObject _frameImage;
+
+    [SerializeField, Range(0f, 1f)]
+    private float _hoverBrightness = 0.8f;
 
     private HandItemController _controller;
     private int _index;
+
     private Color _normalColor = Color.white;
     private bool _hasCachedNormalColor;
 
-    public void Initialize(HandItemController controller, int index)
+    private bool _hasBook;
+    private bool _isSelected;
+    private bool _isPointerOver;
+
+    public void Initialize(
+        HandItemController controller,
+        int index)
     {
         _controller = controller;
         _index = index;
+
         CacheNormalColor();
+        UpdateVisual();
     }
 
     public void SetView(Sprite sprite, bool selected)
@@ -28,23 +43,84 @@ public class HandItemSlotUI : MonoBehaviour, IPointerClickHandler
 
         CacheNormalColor();
 
-        bool hasBook = sprite != null;
+        _hasBook = sprite != null;
+        _isSelected = selected;
+
+        if (!_hasBook)
+        {
+            _isPointerOver = false;
+        }
+
         _bookImage.sprite = sprite;
-        _bookImage.gameObject.SetActive(hasBook);
-        _bookImage.color = selected ? _selectedColor : _normalColor;
+        _bookImage.gameObject.SetActive(_hasBook);
+
+        UpdateVisual();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (eventData.button == PointerEventData.InputButton.Left)
+        if (!_hasBook)
+        {
+            return;
+        }
+
+        if (eventData.button ==
+            PointerEventData.InputButton.Left)
         {
             _controller?.SelectBook(_index);
         }
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!_hasBook)
+        {
+            return;
+        }
+
+        _isPointerOver = true;
+        UpdateVisual();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _isPointerOver = false;
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        if (_frameImage != null)
+        {
+            _frameImage.SetActive(
+                _hasBook && _isSelected
+            );
+        }
+
+        if (_bookImage == null || !_hasBook)
+        {
+            return;
+        }
+
+        if (_isPointerOver)
+        {
+            _bookImage.color = new Color(
+                _normalColor.r * _hoverBrightness,
+                _normalColor.g * _hoverBrightness,
+                _normalColor.b * _hoverBrightness,
+                _normalColor.a
+            );
+        }
+        else
+        {
+            _bookImage.color = _normalColor;
+        }
+    }
+
     private void CacheNormalColor()
     {
-        if (_hasCachedNormalColor || _bookImage == null)
+        if (_hasCachedNormalColor ||
+            _bookImage == null)
         {
             return;
         }
